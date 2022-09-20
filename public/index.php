afb54f0b522f39f9e4d47201d10a741957e088b9
vqck <toridnc@gmail.com>
2022-09-20 21:26:11 +0300

    add new handlers and templates

--- public/index.php
+++ public/index.php
@@ -3,25 +3,39 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    $response->getBody()->write('Welcome to Slim!');
+    return $response;
 });
 
 $app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+    $response->getBody()->write('GET /users');
+    return $response;
 });
 
 $app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+    $response->withStatus(302);
+    return $response;
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
-    return $response->write("Course id: {$id}");
+    $response->getBody()->write("Course id: {$id}");
+    return $response;
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
