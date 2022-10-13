5f2bca0823f11d465be51e1cc97de88f4a23daeb
vqck <toridnc@gmail.com>
2022-10-13 19:41:27 +0300

    added named routes, fixed getting id

--- public/index.php
+++ public/index.php
@@ -22,48 +22,56 @@ $container->set('flash', function () {
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 // WELCOME
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
-});
+})->setName('welcome');
 
 // GET ONE COURSE
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName('course');
 
 // GET FORM FOR POST NEW USER
 $app->get('/users/new', function ($request, $response) {
-    $user['id'] = uniqid;
     $params = [
-        'user' => ['id' => $id, 'name' => '', 'email' => '']
+        'user' => ['name' => '', 'email' => '', 'id' => $id]
     ];
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
-});
+})->setName('createNewUser');
 
 $file = 'src/users.json'; // FILE TO SAVE USERS
 
 // POST NEW USER
-$app->post('/users', function ($request, $response) use ($file) {
+$app->post('/users', function ($request, $response) use ($file, $router) {
     $user = $request->getParsedBodyParam('user');
     $fileUser = json_decode(file_get_contents($file));
+    $user['id'] = uniqid();
     $fileUser[] = $user;
     file_put_contents($file, json_encode($fileUser));
-    return $response->withRedirect('/users', 302);
+    $this->get('flash')->addMessage('success', 'User was added successfully');
+    return $response->withRedirect($router->urlFor('users'), 302);
 
     $params = ['user' => $user];
     return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
-});
+})->setName('postNewUser');
 
 // GET ALL USERS
 $users = ['Mike', 'Mishel', 'Adel', 'Keks', 'Kamila'];
+$users[] = json_decode(file_get_contents(array_values($file)));
 $app->get('/users', function ($request, $response) use ($users) {
+    $messages = $this->get('flash')->getMessages();
     $term = $request->getQueryParam('term');
     $filteredUsers = array_filter($users, fn($user) => str_contains($user, $term) === true);
-    $params = ['users' => $filteredUsers];
+    $params = [
+        'users' => $filteredUsers,
+        'flash' => $messages
+    ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
 
 // GET ONE USER
 $app->get('/users/{id}', function ($request, $response, $args) use ($file) {
@@ -72,6 +80,6 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($file) {
         return $response->withStatus(404);
     }
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user');
 
 $app->run();
