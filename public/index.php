b7ebe5c0e5ec3efa29e08acca4b5a082f6feeb89
vqck <toridnc@gmail.com>
2022-11-02 00:39:00 +0300

    404 error if id does not exist and drawing template if there is, added some comments, updated phtml files

--- public/index.php
+++ public/index.php
@@ -24,6 +24,9 @@ $app = AppFactory::create();
 
 $router = $app->getRouteCollector()->getRouteParser();
 
+// FILE TO SAVE USERS
+$file = 'src/users.json';
+
 // WELCOME
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
@@ -43,47 +46,59 @@ $app->get('/users/new', function ($request, $response) {
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('createNewUser');
 
-// FILE TO SAVE USERS
-$file = 'src/users.json';
-
 // POST NEW USER
 $app->post('/users', function ($request, $response) use ($file, $router) {
     $user = $request->getParsedBodyParam('user');
     $fileUser = json_decode(file_get_contents($file));
+    // Create user unique id and add user to file to save users.
     $user['id'] = uniqid();
     $fileUser[] = $user;
     file_put_contents($file, json_encode($fileUser));
+
     $this->get('flash')->addMessage('success', 'User was added successfully');
     return $response->withRedirect($router->urlFor('users'), 302);
 
     $params = ['user' => $user];
     return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
+
 })->setName('postNewUser');
 
 // GET ALL USERS AND FILTER
-$allUsers = json_decode(file_get_contents($file));
-$users = array_column($allUsers, "name");
-$app->get('/users', function ($request, $response) use ($users) {
-    $messages = $this->get('flash')->getMessages();
+$app->get('/users', function ($request, $response) use ($file) {
+    $allUsers = json_decode(file_get_contents($file));
+    // Get a column with names from a set of records.
+    $names = array_column($allUsers, "name");
     $term = $request->getQueryParam('term');
-    $filteredUsers = array_filter($users, fn($user) => str_contains(strtolower($user), strtolower($term)) === true);
+    // Filter users on request.
+    $filteredUsers = array_filter($names, fn($name) => str_contains(strtolower($name), strtolower($term)) === true);
+    $messages = $this->get('flash')->getMessages();
+
     $params = [
         'users' => $filteredUsers,
         'flash' => $messages
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+
 })->setName('users');
 
 // GET ONE USER
 $app->get('/users/{id}', function ($request, $response, $args) use ($file) {
-    if (str_contains($file, $args['id'])) {
-        return $response->withStatus(404);
+    $id = $args['id'];
+    $allUsers = json_decode(file_get_contents($file));
+    // Get column with names, and as keys of the return array we use values from column "id".
+    $ids = array_column($allUsers, "name", "id");
+
+    // 404 error if id does not exist and drawing template if there is.
+    if (!array_key_exists($id, $ids)) {
+        return $response->write('Page not found')->withStatus(404);
     }
+
     $params = [
-        'id' => $args['id'],
-        'nickname' => 'user-' . $args['id']
+        'id' => $id,
+        'username' => $ids[$id]
     ];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+
 })->setName('user');
 
 $app->run();
